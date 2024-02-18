1f47bdca1105b728a576af69d121f8121a7ffd8e
SamuelCastroMeras <samuelcastromeras@gmail.com>
2024-02-18 17:54:26 +0100

    Updated the code and added speach to text

---
+++ audio.py
@@ -0,0 +1,44 @@
+import wave
+import pyaudio
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+client = OpenAI(api_key=os.environ.get("API_KEY",""))
+
+def speech_to_text():
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+    filename = "output.wav"
+    p = pyaudio.PyAudio()
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []
+
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    audio_file = open("output.wav",'rb')
+    transcript = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file
+    )
+    return transcript.text
--- main.py
+++ main.py
@@ -1,48 +1,54 @@
 from openai import OpenAI
 import os
+from audio import speech_to_text
 import json
 from dotenv import load_dotenv
 from clase import Chat
 load_dotenv()
-client = OpenAI(api_key=os.environ.get("API_KEY",""))
+client = OpenAI(api_key=os.environ.get("API_KEY", ""))
+
 
 prompt1 = """
         Bienvenido al juego de BlackStories, donde tu sabras una historia y tendras que hacer que el usuario adivine la historia unicamente con preguntas de si o no. 
         Unicamente puedes responder si o no. Si la pregunta no se puede responder con un si o un no, dile al usuario que la pregunta no la puedes responder. 
-        Un hombre fue apuñalado con un carambano de hielo que su asesino habia introducido en un termo. 
+        Un hombre fue apuñalado con un carambano de hielo que su asesino habia introducido en un termo.
         Poco despues del ataque, el carambano se derritio por el calor de la sauna, por lo que el arma del crimen nunca fue hallada. 
         Responde con un json. Se guardan las claves content y reason.
         Esta es la plantilla:
-        { 
+        {
             reason: "Porque has llegado a la conclusion de tu respuesta"
             relevante: "Responder true o false, dependiendo de si es relevante para resolver el caso o no"
             valid: "Responder true o false dependiendo de si puedes responder a esa pregunta con "si" o "no" o no puedes hacerlo"
-            content: "La respuesta que quieres mostrarle al usuario"
-        
+            content: "true o false dependiendo de si la respuesta es un si o un no"
+
         }
         Ejemplo pregunta: "¿El hombre esta muerto?"
-        Respuesta:
         {
             reason: "El hombre esta muerto segun la historia, por lo que la respuesta a la pregunta es sí"
             relevante: true
             valid: true
-            content: "Si"
+            content: true
         }
         Ejemplo pregunta: "¿El hombre es rubio?"
-        Respuesta:
         {
             reason:"Esa informacíon es desconocida e irrelevante"
             relevante: false
             valid: true
-            content: "Esa pregunta es irrelevante"
+            content: false
         }
         Ejemplo pregunta: "¿Como le han matado?"
-        Respuesta:
         {
             reason:"No puedo responder a esa pregunta porque tendría que responder algo que no es un si o un no"
             relevante: true
             valid: false
-            content: "No puedo responderte a esa pregunta"
+            content: false
+        }
+        Ejemplo pregunta: "asdasdasdasd"
+        {
+            reason:"No puedo responder a esa pregunta porque no la entiendo"
+            relevante: false
+            valid: false
+            content: false
         }
         """
 
@@ -50,12 +56,12 @@ prompt2 = """
         Tienes que recibir la información que el jugador ya ha descubierto y guardarlo, como si fuesen logros.
         Responde con un json. Se guardan en milestones de clave bool.
         Las milestones son estas:
-            know_weapon (para cuando descubra que el arma es un carambano de hielo), 
-            know_wound(cuando sepa que ha seido apuñalado), 
+            know_weapon (para cuando descubra que el arma es un carambano de hielo),
+            know_wound(cuando sepa que ha seido apuñalado),
             know_death_cause (ha adivinado que la persona se ha desangrado),
-            
 
-        La respuesta tiene que ser un json con el siguiente formato: 
+
+        La respuesta tiene que ser un json con el siguiente formato:
         {
             reason: "Why the different milestones are discovered"
             know_weapon: false or true,
@@ -63,47 +69,71 @@ prompt2 = """
             know_death_cause: false or true,
         }
 
-        Ejemplo pregunta: Le han apuñalado? Resultado: Si
+        Ejemplo pregunta: Le han apuñalado?
         Response:
         {
             reason: "La respuesta demuestra que el hombre ha sido apuñalado"
             know_weapon: false,
             know_wound: true,
             know_death_cause: false,
-        }        
-        Ejemplo pregunta: Se ha desangrado? Resultado: Si
+        }
+        Ejemplo pregunta: Se ha desangrado?
         Response:
         {
             reason: "La respuesta demuetra que el hombre se ha desangrado"
             know_weapon: false,
             know_wound: false,
             know_death_cause: true,
-        }        
-        Ejemplo pregunta: Le han matado con un carambano? Resultado: Si
+        }  
+        Ejemplo pregunta: Le han matado con un carambano?
         Response:
         {
-            reason: "La respuesta demuestra que el hombre ha sido asesinado con un acrambano"
+            reason: "La respuesta demuestra que el hombre ha sido asesinado con un carambano"
             know_weapon: true,
             know_wound: false,
             know_death_cause: false,
-        }        
+        }
         """
 
-milestones={}
+milestones = {"know_weapon": False,
+            "know_wound": False,
+            "know_death_cause": False
+            }
+completed_milestones = 0
 print("Eres el jugador de un juego llamado Black Stories. ChatGPT va a tener una historia y tu tienes que descubrir lo que ha ocurrido. Para lograr descubrir lo que ha pasado tendras que hacer preguntas, y solo te podran responder con un si o un no.")
 print(" Hay un hombre muerto en una sauna, esta tumbado, junto a el hay un termo. ")
+response_finished = True
 chat = Chat(prompt1)
 chat2 = Chat(prompt2)
 while True:
-    pregunta = input("Realiza tu pregunta: ")
-    response1=chat.send_message_json(pregunta)
+    input("Pulsa enter para grabar")
+    pregunta = speech_to_text()
+    print(pregunta)
+    response1 = chat.send_message_json(pregunta)
+    if not response1["valid"]:
+        print("Tienes que hacer una pregunta de si o no")
+        response_finished = True
+        continue
     if not response1["relevante"]:
         print("Esa pregunta es irrelevante")
+        response_finished = True
         continue
-    if not response1["valid"]:
-        print("tienes que hacer una pregunta de si o no")
-        continue
-    print(response1["content"])
+
+    if response1["content"]:
+        print("Si.")
+        response_finished = True
+    else:
+        print("No.")
+        response_finished = True
+    for valor in milestones.values():
+        if valor:
+            completed_milestones += 1
+        else:
+            continue
+    if completed_milestones == 2:
+        print("Muy bien, has resuelto el caso")
+        break
+
 
 
 
@@ -115,6 +145,3 @@ while True:
 # contenido_respuesta = json.loads(contenido_respuesta)
 # contenido_respuesta2 = json.loads(contenido_respuesta2)
 # print(contenido_respuesta["content"])
-# if contenido_respuesta2["finished"]:
-#     print("Muy bien, has resuelto el caso")
-#     break
